41aae57b1aa041ebd3b3314bf825e7d8a3cfbc1e
essivekkeli <essi.vekkeli@gmail.com>
2024-07-09 14:41:32 +0300

    1.6

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -1,4 +1,4 @@
-const Header = (props) => {
+/*const Header = (props) => {
   return (
     <div>
       <h1>{props.name}</h1>
@@ -63,6 +63,76 @@ const App = () => {
       <Total parts={course.parts} />
     </div>
   )
+}*/
+
+const Header = (props) => {
+  return (
+    <div>
+      <p> {props.name}</p>
+    </div>
+  )
 }
+
+const Button = ({ handleClick, text }) => {
+  <button onClick={handleClick}>
+    {text}
+  </button>
+}
+
+const Statistics = (props) => {
+  return (
+    <div>
+      <p>{props.title}</p>
+    </div>
+  )
+}
+
+import { useState } from 'react'
+
+const App = () => {
+  const label1 = "Give feedback"
+  const label2 = "Statistics"
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+  
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
+
+  return (
+    <div>
+      <Header name={label1} />
+
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+      <br></br>
+      
+      <Header name= {label2}/>
+      
+      <Statistics title={'Good'} /> {good}
+      <Statistics title={'Neutral'}/> {neutral}
+      <Statistics title={'Bad'}/> {bad}
+     
+      
+    </div>
+  )
+}
+
+
 
 export default App
