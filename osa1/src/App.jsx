6f6bec5eeaeedd785fd24aebdd403f8454300baa
essivekkeli <essi.vekkeli@gmail.com>
2024-07-11 10:39:50 +0300

    1.12

--- osa1/src/App.jsx
+++ osa1/src/App.jsx
@@ -65,6 +65,9 @@ const App = () => {
   )
 }*/
 
+
+
+/*
 import { useState } from 'react'
 
 const Header = (props) => {
@@ -88,7 +91,7 @@ const StatisticLine = (props) => {
     <table>
       <tbody>
         <tr>
-          <td>{props.text} </td>
+          <td>{props.text}</td>
           <td>{props.value}</td>
           <td>{props.pros}</td>
         </tr>
@@ -181,5 +184,67 @@ const App = () => {
   )
 }
 
+
+export default App
+
+*/
+
+
+
+import { useState } from 'react'
+
+const Button = ({ handleClick, text }) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Anecdote = (props) => {
+  if (!props.selected) {
+    console.log("Vielä ei ole klikattu")
+    return (
+      <div>
+        Button is not clicked
+      </div>
+    )
+  } else {
+    console.log("Nyt on klikattu")
+    return (
+      <div>
+        <p>{props.selected}</p>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const handleClick = () => {
+    const randomSelection = Math.floor(Math.random()* anecdotes.length);
+    setSelected(anecdotes[randomSelection])
+  }
+
+
+  return (
+    <div>
+      <Anecdote selected={selected} />
+      <Button handleClick={handleClick} text='Next anecdote' />
+    </div>
+  )
+}
 
 export default App
