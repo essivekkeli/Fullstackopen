d04d16731d013de4f212204640654702a046c286
essivekkeli <essi.vekkeli@gmail.com>
2024-07-19 14:23:45 +0300

    2.4.

--- osa2/src/App.jsx
+++ osa2/src/App.jsx
@@ -43,39 +43,63 @@ const Course = (props) => {
   console.log(props, "Course module")
   return (
     <div>
-      <Header name={props.course.name} />
-      <Content parts={props.course.parts} />
-      <Total total={props.course.parts} />
+      <Header name={props.courses.name} />
+      <Content parts={props.courses.parts} />
+      <Total total={props.courses.parts} />
     </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course course={course} />
+      <Course courses={courses}/>
+
     </div>
   )
 }
