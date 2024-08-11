402e2069094de700bc693eaf930d4cea68e01c5f
Essi Vekkeli <essivek@uef.fi>
2024-08-11 15:58:41 +0300

    2.15

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-
+import personService from './Persons'
 
 const Person = ({ addName, newName, number, handleName, handleNumber }) => {
   return (
@@ -13,7 +13,7 @@ const Person = ({ addName, newName, number, handleName, handleNumber }) => {
           number: <input value={number} onChange={handleNumber} />
         </div>
         <div>
-          <button type="submit" >add</button>
+          <button type="submit" >Add</button>
         </div>
       </form>
 
@@ -21,26 +21,27 @@ const Person = ({ addName, newName, number, handleName, handleNumber }) => {
   )
 }
 
-const Info = ({filter, persons}) => {
+const Info = ({ filter, persons, handleDelete }) => {
   const showinfo = filter === ''
     ? persons
     : persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
 
   return (
     <div >
-        {showinfo.map(person =>
-          <p key={person.name}>{person.name} {person.number}</p>
-        )}
-      </div>
+      {showinfo.map(person =>
+        <p key={person.name}>{person.name} {person.number} <button onClick={() => handleDelete(person)}>Delete</button></p>
+
+      )}
+    </div>
   )
 }
 
-const Filter = ({handleFilter}) => {
+const Filter = ({ handleFilter }) => {
   return (
     <div>
       <form>
         <div>
-          filter shown with <input onChange={handleFilter}/>
+          filter shown with <input onChange={handleFilter} />
         </div>
       </form>
     </div>
@@ -49,76 +50,101 @@ const Filter = ({handleFilter}) => {
 
 const App = () => {
   const [persons, setPersons] = useState([])
-
   const [newName, setNewName] = useState('')
-
   const [number, setNumber] = useState('')
-
   const [filter, setFilter] = useState('')
 
-
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
-        console.log('promise fulfilled')
         setPersons(response.data)
       })
   }, [])
-  
+
 
 
   const addName = (event) => {
     event.preventDefault()
-    console.log('nappia painettu', event.target)
+    console.log('Add nappia painettu', event.target)
 
-    const nameArray = persons.map(nimi => nimi.name)
-    console.log('Nimilista ', nameArray)
-    console.log('newName on', newName)
+    const existingPerson = persons.find(p => p.name === newName)
 
+    if (existingPerson) {
+      console.log('existingperson tulostuu')
+      const userConfirmsUpdate = window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)
 
-    if (nameArray.includes(newName)) {
-      window.alert(newName + ' is already added to phonebook')
-      console.log(newName, 'on jo taulukossa')
-    }
-    else {
-      addPerson(event)
-      console.log('taulukon sisältö', persons)
+      if (userConfirmsUpdate) {
+        console.log('usercomfirmed tulostuu')
+        const makeUpdate = { ...existingPerson, number: number }
+
+        axios
+        .put(`http://localhost:3001/persons/${existingPerson.id}`, makeUpdate)
+        .then(response => {
+          setPersons(persons.map(p => p.id !== existingPerson.id ? p : response.data))
+          setNewName('')
+          setNumber('')
+        })
+        .catch(error => {
+          console.log(`There was an error updating the person's number! ${error}`);
+        });
+      }
+      
+    } else {
+      const personObject = {
+        name: newName,
+        number: number
+      }
+      axios
+        .post('http://localhost:3001/persons', personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNumber('')
+        })
+        .catch(error => {
+          console.log(`There was an error adding the person! ${error}`)
+        })
+
     }
   }
 
 
-  const addPerson = (event) => {
-    console.log(event.target.value)
-    event.preventDefault()
-    const personObject = {
-      name: newName,
-      number: number
+  const handleDelete = (person) => {
+    console.log('pääseekö handleDeleteen?')
+    const userConfirmation = window.confirm(`Delete ${person.name} ?`)
+
+    if (userConfirmation) {
+      console.log('Delete nappia painettu')
+
+      axios
+        .delete(`http://localhost:3001/persons/${person.id}`)
+        .then(() => {
+          setPersons(persons.filter(p => p.id !== person.id))
+        })
+        .catch(error => {
+          console.log(`There was an error deleting the person! ${error}`)
+        })
+
+      console.log('Contact info deleted')
+    } else {
+      console.log('Delete canceled')
     }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNumber('')
   }
 
 
   const handleName = (event) => {
-    event.preventDefault()
-    console.log(event.target.value)
     setNewName(event.target.value)
   }
 
   const handleNumber = (event) => {
-    event.preventDefault()
-    console.log(event.target.value)
     setNumber(event.target.value)
   }
 
   const handleFilter = (event) => {
-    event.preventDefault()
-    console.log(event.target.value)
     setFilter(event.target.value)
   }
+
 
   return (
     <div>
@@ -133,8 +159,8 @@ const App = () => {
       />
 
       <h2>Numbers</h2>
-      <Info 
-        persons={persons} filter={filter}
+      <Info
+        persons={persons} filter={filter} handleDelete={handleDelete}
       />
     </div>
   )
